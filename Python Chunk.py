97b234266aed767422696c35900ef0003a693f87
Zazerbeam <118231580+Zazerbeam@users.noreply.github.com>
2023-07-06 21:41:31 -0700

    Moved code
    whoohooo

--- Python Chunk.py
+++ Python Chunk.py
@@ -17,10 +17,12 @@ def main():
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
     """
-    creds = None
+    #with open("api.txt", "rw") as key:
+    #    creds = key
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
+    creds = None
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
     # If there are no (valid) credentials available, let the user log in.
